317b303d6ad776f6cb23d7f7c35c62d62af3f6c6
qkey <petrovsergy@outlook.com>
2019-12-21 13:45:59 +0500

    use session

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const expressWs = require('express-ws')(app);
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.static("static"));
+app.use(express.json());
+
+
+
+
 
 app.listen(port, (err) => {
     if (err) {
