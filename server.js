54927e9d53efc6db974b263c840bc0a78f2af7d5
Artemiy <38810090+OptimumDev@users.noreply.github.com>
2019-12-21 12:33:33 +0500

    js init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.static("static"));
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+
+    console.log(`server is listening on ${port}`);
+});
